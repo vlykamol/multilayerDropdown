6402c5be2d5f98a00138cf6a2d1780105ac41c7d
valaykamol <vlykamol@outlook.com>
2022-10-31 10:03:00 +0530

    second commit

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 import './style.css'
 
-const navItems = document.querySelectorAll('.mainNavLink')
-const innerLists = document.querySelectorAll('.innerList')
+// const navItems = document.querySelectorAll('.mainNavLink')
+// const innerLists = document.querySelectorAll('.innerList')
 
-const handelList = (e) => {
-  const innerList = e.target.childNodes[1]
-  if(innerList){
-    innerList.classList.add("active")
-    console.log('inner list ---- ', innerList);
-    innerList.addEventListener('mouseenter', handelList)
-  }
-}
+// const handelList = (e) => {
 
-navItems.forEach(navItem => {
-  navItem.addEventListener('mouseenter', handelList)
-})
+//   const innerList = e.target.childNodes[1]
+//   if(innerList){
+//     innerList.classList.add("active")
+//     console.log('inner list ---- ', innerList);
+//     innerList.addEventListener('mouseenter', handelList)
+//   }
+// }
+
+// navItems.forEach(navItem => {
+//   navItem.addEventListener('mouseenter', handelList)
+// })
+
+// navItems.forEach(navItem => {
+//   navItem.addEventListener('mouseleave', () =>{
+//     const activeLists = document.querySelectorAll('.active')
+//     activeLists.forEach(al => {
+//     console.log('active lists', al);
+//     })
+//   })
+// })
 
 
 
